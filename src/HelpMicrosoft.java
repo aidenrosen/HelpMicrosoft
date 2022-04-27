373d619e82000102a9e8b67ae27c33caaf488aac
aidenrosen <aidenrrosen@gmail.com>
2022-04-27 16:04:07 -0400

    Started GithubAPI process

--- src/HelpMicrosoft.java
+++ src/HelpMicrosoft.java
@@ -1,3 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HelpMicrosoft
 {
+        private static Scanner tokenReader;
+    public static void main(String[] args)
+    {
+        try
+        {
+            tokenReader = new Scanner(new File("token.txt"));
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new RuntimeException(e);
+        }
+
+        String myToken = tokenReader.nextLine();
+
+        Scanner scanner = new Scanner(System.in);
+        String user = scanner.nextLine();
+
+        GithubHelper githubHelper = new GithubHelper(user, myToken);
+        githubHelper.createRepo("testRepo");
+    }
+
 }
